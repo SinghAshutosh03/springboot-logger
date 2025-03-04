22a6b240fbf606c8d464f5f3b86d53ec0aa443fa
SinghAshutosh03 <ashutoshsingh030302@gmail.com>
2025-03-04 12:38:01 +0530

    Implemented UC4: POST request with JSON body

--- src/main/java/com/example/assign/controller/HelloController.java
+++ src/main/java/com/example/assign/controller/HelloController.java
@@ -1,15 +1,21 @@
 package com.example.assign.controller;
 
+import com.example.assign.dto.UserDTO;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping("/param/{name}")
-    public String sayHelloWithPath(@PathVariable String name) {
-        return "Hello " + name + " from BridgeLabz";
-    }
+//    @GetMapping("/param/{name}")
+//    public String sayHelloWithPath(@PathVariable String name) {
+//        return "Hello " + name + " from BridgeLabz";
+//    }
+@PostMapping("/post")
+public String sayHelloWithPost(@RequestBody UserDTO user) {
+    return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+}
+
 
 
 }
